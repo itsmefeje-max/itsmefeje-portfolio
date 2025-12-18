dad1ce21f9c5fd363c1165df083362b074aa3111
itsmefeje-max <itsmefeje@gmail.com>
2025-12-18 22:51:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+// main.js
+// This file will control advanced visuals later (Aurora background)
+
+// For now, we just confirm JavaScript is connected
+console.log("Itsmefeje portfolio loaded successfully.");
 
